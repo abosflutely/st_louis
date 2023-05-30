27559ebe464181b978c745bebde6c3044d78edaf
abosflutely <achen315@student.fuhsd.org>
2023-05-29 20:10:18 -0700

    start new repo

---
+++ main.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv('/Users/averychen/Documents/st_louis/crimedata2023.csv')
+
+data = df.dropna(subset='address')
+data.to_csv('stlouis_2023')
+# print(data.head())
